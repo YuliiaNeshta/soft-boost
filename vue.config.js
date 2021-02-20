b3b8882b641ff0d24216ead1ce0f6a72354d1243
YuliiaNeshta <julija.neshta@ro.ru>
2021-02-20 14:55:10 +0200

    deploy

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,8 @@ module.exports = {
 					prependData: `@import "@/assets/scss/_variables.scss";`
 			}
 		}
-	}
+	},
+	publicPath: process.env.NODE_ENV === 'production'
+    ? '/soft-boost/'
+    : '/'
 };
